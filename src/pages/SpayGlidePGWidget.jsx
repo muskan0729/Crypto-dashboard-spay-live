02d70e8865f1c89c8e9f5dd485bdf42ee01515a8
Intern-003 <intern@spay.live>
2025-12-20 18:44:25 +0530

    done

--- src/pages/SpayGlidePGWidget.jsx
+++ src/pages/SpayGlidePGWidget.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ReviewPaymentModal from "../components/GlideModal/ReviewPaymentModal";
 import logo from "../images/logo.png";
-import paymentGatewayBg from "../images/login-background.jpg";
+import paymentGatewayBg from "../images/lgoinbg.jpg";
 import { PaymentProvider } from "../contexts/PaymentContext";
 import { useGlidePay } from '@paywithglide/glide-react';
 import { paymentService } from "../services/paymentService"; // API calls
